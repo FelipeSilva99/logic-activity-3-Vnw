8596972207b0ad5a1e34a0e3d7c7935218732d31
FelipeSilva99 <felipe.rl.silva@hotmail.com>
2021-05-22 19:45:21 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+for (let number1 = 1; number1 < 11; number1++){
+    console.log(number1)
+}
+
+let number2 = 10
+while (number2 > 0){
+    console.log(number2)
+    number2--
+}
+
+let number3 = 1
+while(number3 < 100){
+    console.log(number3)
+    number3+=2
+}
+
+let number4 = 0
+while(number4 <= 100){
+    console.log(number4)
+    number4+=2
+}
